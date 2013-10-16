90aa72a2233b29105a6452ea43c36b2badfed927
gotoflect <goto@f.flect.co.jp>
2013-10-16 18:08:25 +0900

    commit guide7

--- app/models/User.java
+++ app/models/User.java
@@ -4,13 +4,17 @@ import java.util.*;
 import javax.persistence.*;
  
 import play.db.jpa.*;
+import play.data.validation.*;
  
 @Entity
-//@Table(name=Ågblog_userÅh)
 public class User extends Model {
- 
+    @Email
+    @Required
     public String email;
+    
+    @Required
     public String password;
+    
     public String fullname;
     public boolean isAdmin;
     
@@ -23,5 +27,8 @@ public class User extends Model {
     public static User connect(String email, String password) {
         return find("byEmailAndPassword", email, password).first();
     }
- 
+
+    public String toString() {
+        return email;
+    }
 }
